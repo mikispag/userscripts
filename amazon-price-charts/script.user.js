c455cd0b3cbe35dabd79fbbe8b4504bb35c77523
Michele Spagnuolo <mikispag@gmail.com>
2020-11-23 20:48:16 +0100

    amazon-price charts: update to 1.0.4, better image sizes.

--- amazon-price-charts/script.user.js
+++ amazon-price-charts/script.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name           Amazon CamelCamelCamel + Keepa Price Charts
-// @version        1.0.3
-// @description    Add a CamelCamelCamel and Keepa price charts to Amazon product pages.
-// @author         miki.it
-// @namespace      null
-// @homepage       https://github.com/mikispag/userscripts/
-// @include        https://www.amazon.*/*
-// @include        https://smile.amazon.*/*
+// @name            Amazon CamelCamelCamel + Keepa Price Charts
+// @version         1.0.4
+// @description     Add a CamelCamelCamel and Keepa price charts to Amazon product pages.
+// @author          miki.it
+// @namespace       null
+// @homepage        https://github.com/mikispag/userscripts/
+// @include         https://www.amazon.*/*
+// @include         https://smile.amazon.*/*
 // ==/UserScript==
 
 var tld = document.domain.split(".").pop();
@@ -33,8 +33,8 @@ var camelChartContainer = document.createElement("div");
 var camelLink = document.createElement("a");
 camelLink.target = "_blank";
 camelLink.href = "https://" + country + ".camelcamelcamel.com/product/" + asin;
-var camelChart = new Image(600, 250);
-camelChart.src = "https://charts.camelcamelcamel.com/" + country + "/" + asin + "/amazon-new-used.png?force=1&zero=0&w=600&h=250&desired=false&legend=1&ilt=1&tp=all&fo=0";
+var camelChart = new Image(500, 400);
+camelChart.src = "https://charts.camelcamelcamel.com/" + country + "/" + asin + "/amazon-new-used.png?force=1&zero=0&w=500&h=400&desired=false&legend=1&ilt=1&tp=all&fo=0";
 camelLink.appendChild(camelChart);
 camelChartContainer.appendChild(camelLink);
 
@@ -42,7 +42,7 @@ var keepaChartContainer = document.createElement("div");
 var keepaLink = document.createElement("a");
 keepaLink.target = "_blank";
 keepaLink.href = "https://keepa.com/#!product/8-" + asin;
-var keepaChart = new Image(600, 250);
+var keepaChart = new Image(500, 200);
 keepaChart.src = "https://graph.keepa.com/pricehistory.png?used=1&asin=" + asin + "&domain=" + tld;
 keepaLink.appendChild(keepaChart);
 keepaChartContainer.appendChild(keepaLink);
